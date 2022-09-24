b847c0e3e1ea941a0388152ed310f59d44e27b14
kaikai-23 <gate.sea0203@gmail.com>
2022-09-24 10:27:02 +0900

    マイグレーションとリレーション設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the posts for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        //hasmanyは紐付いたデータを返す、1つ目Postモデルの
+        return $this->hasMany(Post::class);
+    }
+
 }
