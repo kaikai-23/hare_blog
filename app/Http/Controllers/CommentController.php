ec9986ca40810805750e47ab9d1797aa6b425d37
kaikai-23 <gate.sea0203@gmail.com>
2022-09-25 10:38:45 +0900

    コメント編集、コメント削除

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -65,6 +65,7 @@ class CommentController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Comment  $comment
+     * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
      */
     public function show(Comment $comment)
@@ -75,12 +76,13 @@ class CommentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Models\Post  $post
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function edit(Comment $comment)
+    public function edit(Post $post, Comment $comment)
     {
-        //
+        return view('comments.edit', compact('post', 'comment'));
     }
 
     /**
@@ -90,19 +92,55 @@ class CommentController extends Controller
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function update(CommentRequest $request, Comment $comment)
+    public function update(CommentRequest $request,Post $post, Comment $comment)
     {
-        //
+        if ($request->user()->cannot('update', $comment)) {
+            return redirect()->route('posts.show', $post)
+                ->withErrors('自分のコメント以外は更新できません');
+        }
+
+        $comment->fill($request->all());
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            // 更新
+            $comment->save();
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post)
+            ->with('notice', 'コメントを更新しました');
     }
 
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Models\Post  $post
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Comment $comment)
+    public function destroy(Post $post,Comment $comment)
     {
-        //
+        //トランザクション開始
+        DB::beginTransaction();
+        try{
+            $comment->delete();
+            
+        //トランザクション終了(成功)    
+            DB::commit();
+        }catch(\Exception $e){
+            //トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+        return redirect()->route('posts.show', $post)
+        ->with('notice', 'コメントを削除しました');
     }
 }
