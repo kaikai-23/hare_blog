7d2f487a512b22a7851fa3c257bf3df1f0e23d8b
kaikai-23 <gate.sea0203@gmail.com>
2022-10-09 18:18:35 +0900

    課題終了

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,5 +56,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
-
+    public function identityProvider()
+    {
+        return $this->hasOne(IdentityProvider::class);
+    }
 }
